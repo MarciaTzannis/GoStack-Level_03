eb615881388b15dff898023041e107238411a2a7
MarciaTzannis <marciatzannis@gmail.com>
2021-02-05 13:29:03 -0300

    Configurando o StatusBar

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import {View} from 'react-native';
+import 'react-native-gesture-handler';
 
-const App: React.FC = () => <View />;
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+    <StatusBar barStyle="light-content" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+);
 
 export default App;
